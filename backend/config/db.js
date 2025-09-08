06c4170012e6cecd6e3f4c4ca9df280adce9001e
Jarin Tasnim Dia <142290896+dia-j@users.noreply.github.com>
2025-09-09 00:37:24 +0600

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-// filename: test-db.js
+
 import mongoose from 'mongoose';
 
 export const connectDB = async (uri = process.env.MONGO_URI) => {
@@ -17,3 +17,34 @@ export const connectDB = async (uri = process.env.MONGO_URI) => {
     throw err;
   }
 };
+
+/*import mongoose from 'mongoose';
+
+export const connectDB = async (uri = process.env.MONGO_URI) => {
+  if (!uri) {
+    throw new Error('MONGO_URI not set. Add backend/.env with MONGO_URI');
+  }
+
+  try {
+    console.log('Attempting mongoose.connect...');
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 10000, // short timeout for dev
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    throw err;
+  }
+};
+
+*/
+
+
+
+
+
+
+
+
+
+
